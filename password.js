adfd7d000254160fa6592fc499a6933270df876e
Aiden Kampman <aidenshanekampman@gmail.com>
2022-02-11 12:18:36 -0700

    Cant get special character pass

---
+++ password.js
@@ -0,0 +1,28 @@
+console.log("Welcome to Aidens password validator!")
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+//     function containsSpecialChars(input) {
+//         const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+//         return specialChars.test(input);
+// }
+    
+    reader.question("What is your password?", function(input){
+    if(input.length < 10){
+        console.log("Your password is not long enough")
+    }
+        else if(input.length > 20){
+                console.log("Your password must not exceed 20 characters!")
+            }
+            else if(!/\d/.test(input)){
+                 console.log("Your password does not have a number.")
+                 console.log('false')
+                        //  }else if (constainsSpecialChars(input) = false){
+                        //      console.log('Your password does not contain a special character')
+                }else console.log('Your password was succefully saved')
+            })
+            
