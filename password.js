31fddcb4a719df5d57a0ec452e2c264112bba1d2
Aiden Kampman <aidenshanekampman@gmail.com>
2022-02-11 15:36:33 -0700

    Added capital letter requirement

--- password.js
+++ password.js
@@ -11,6 +11,10 @@ function containsSpecialChars(input) {
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     return specialChars.test(input);
 }
+function toLowerCase(input){
+    return input == input.toLowerCase() && input != input.toUpperCase();
+}
+
 reader.question("What is your password?", function(input){
     if(input.length < 10){
         console.log("Your password is not long enough")
@@ -24,14 +28,17 @@ reader.question("What is your password?", function(input){
                          }
                         else if (containsSpecialChars(input) === false){
                              console.log('Your password does not contain a special character')
-                }else console.log('Your password was succefully saved')
-            })
+                        }else if (toLowerCase(input)){
+                            console.log('Your password needs a capital letter')
+                        }else console.log('Your password was succesfully saved')
+            reader.close()
+        })
             
 
 
 // Below text does not matter but keeping for my own notes
 
-// let me = "Workplz!"
+// let me = "wokr!"
 
 //             function containsSpecialChars(me) {
 //                 const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
@@ -40,3 +47,10 @@ reader.question("What is your password?", function(input){
 //               if (containsSpecialChars(me) === false){
 //               console.log('this is working')
 //               }else console.log ('just work')
+
+// function toLowerCase(me){
+//     return me == me.toLowerCase() && me != me.toUpperCase();
+// }
+// if (toLowerCase(me)){
+//     console.log(' the string needs a capital letter')
+// }else console.log('the string contains a capital letter')
